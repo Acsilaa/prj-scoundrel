19d60cdabef9fe6b62c9101dcb7798f4b87f9586
Acsilaa-mac <galeengames@gmail.com>
2026-06-04 11:30:45 +0200

    refactor: zustand

--- src/lib/deckofcards.ts
+++ src/lib/deckofcards.ts
@@ -1,52 +1,80 @@
+import { create } from "zustand";
 import type { CardCode } from "../types/types";
 
 const apibase = "https://deckofcardsapi.com/api/";
-const rawget = async (uri: string) => {return (await fetch(apibase + uri)).json()};
-const newdeck = async () => {
-    const received = await rawget('deck/new/shuffle?deck_count=1&remove=JOKER_BLACK,JOKER_RED,JD,QD,KD,AD,JH,QH,KH,AH') as ShuffleResponse;
-    DeckOfCardsAPI.deck_id = received.deck_id;
-}
-const draw = async (amount: number) => {
-    const response =  (await rawget('deck/' + (DeckOfCardsAPI.deck_id ?? 'new') + '/draw?count=' + amount) as DrawResponse);
-    DeckOfCardsAPI.remaining = response.remaining;
-    return response.cards;
-}
-const tobottom = async (cardCodes: CardCode[]) => {
-    const response = await rawget('deck/' + DeckOfCardsAPI.deck_id + '/return/?cards=' + (cardCodes.join(',')));
-    DeckOfCardsAPI.remaining = response.remaining
-}
-export const DeckOfCardsAPI: {
-    rawget: (uri:string) => Promise<any>,
-    deck_id: null|string,
-    newdeck: ()=>Promise<void>,
-    draw: (amount: number) => Promise<CardDraw[]>,
-    tobottom: (cardCodes: CardCode[]) => Promise<void>,
-    remaining: number,
-} = {
-    rawget: rawget,
-    deck_id: null,
-    newdeck: newdeck,
-    draw: draw,
-    tobottom: tobottom,
-    remaining: 52,
-}
+
+const rawget = async (uri: string) => {
+  return (await fetch(apibase + uri)).json();
+};
 
 export type ShuffleResponse = {
-    success: boolean,
-    deck_id: string,
-    shuffled: boolean,
-    remaining: number,
-}
+  success: boolean;
+  deck_id: string;
+  shuffled: boolean;
+  remaining: number;
+};
 
 export type DrawResponse = {
-    success: boolean,
-    cards: CardDraw[],
-    remaining: number,
-}
+  success: boolean;
+  cards: CardDraw[];
+  remaining: number;
+};
 
 export type CardDraw = {
-    code: CardCode,
-    image: string,
-    value: number,
-    suit: string,
-}
+  code: CardCode;
+  image: string;
+  value: number;
+  suit: string;
+};
+
+type DeckState = {
+  deckId: string | null;
+  remaining: number;
+
+  newDeck: () => Promise<void>;
+  draw: (amount: number) => Promise<CardDraw[]>;
+  toBottom: (cardCodes: CardCode[]) => Promise<void>;
+};
+
+export const useDeckOfCards = create<DeckState>((set, get) => ({
+  deckId: null,
+  remaining: 52,
+
+  newDeck: async () => {
+    const res = (await rawget(
+      "deck/new/shuffle?deck_count=1&remove=JOKER_BLACK,JOKER_RED,JD,QD,KD,AD,JH,QH,KH,AH"
+    )) as ShuffleResponse;
+
+    set({
+      deckId: res.deck_id,
+      remaining: res.remaining,
+    });
+  },
+
+  draw: async (amount: number) => {
+    const deckId = get().deckId ?? "new";
+
+    const res = (await rawget(
+      `deck/${deckId}/draw?count=${amount}`
+    )) as DrawResponse;
+
+    set({
+      remaining: res.remaining,
+    });
+
+    return res.cards;
+  },
+
+  toBottom: async (cardCodes: CardCode[]) => {
+    const deckId = get().deckId;
+    if (!deckId) return;
+
+    const res = await rawget(
+      `deck/${deckId}/return/?cards=${cardCodes.join(",")}`
+    );
+
+    set({
+      remaining: res.remaining,
+    });
+  },
+}));
