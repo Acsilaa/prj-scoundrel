6b2ef14b0904edea1cc3072aadbae6e1b5c97817
Acsilaa-mac <galeengames@gmail.com>
2026-06-06 13:32:17 +0200

    fix: hp=14

--- src/components/Game/HealthDisplayer.tsx
+++ src/components/Game/HealthDisplayer.tsx
@@ -4,7 +4,6 @@ import PlayingCard from "../PlayingCard/PlayingCard";
 
 export default function HealthDisplayer ({health}:{health: number|undefined}){
     if(health === undefined) return null;
-
     if(health == 1) return <div className="h-[200px] w-36 bg-black/50 shadow-lg shadow-black relative flex items-center justify-center">
         <PlayingCard interactable={false} cardCode={`X2` as CardCode} />
     </div>
@@ -15,7 +14,7 @@ export default function HealthDisplayer ({health}:{health: number|undefined}){
 
     return <div className="h-[200px] w-36 bg-black/50 shadow-lg shadow-black relative flex items-center justify-center">
         <PlayingCard interactable={false} cardCode={`${numberToLetter(Math.min(health,(health > 14 ? 13 : 14)))}H` as CardCode} />
-        {health > 13 && 
+        {health > 14 && 
             <PlayingCard interactable={false} cardCode={`${numberToLetter(health-13)}H` as CardCode} className="absolute left-[calc(50%+16px)] top-[calc(50%+8px)] -translate-x-1/2 -translate-y-1/2" />
         }
     </div>
