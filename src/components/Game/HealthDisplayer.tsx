c3d9e2b34173878604b1e29b9fadf0d8a9a397fe
Acsilaa <galeengames@gmail.com>
2026-06-06 02:04:59 +0200

    health display fix

--- src/components/Game/HealthDisplayer.tsx
+++ src/components/Game/HealthDisplayer.tsx
@@ -10,9 +10,9 @@ export default function HealthDisplayer ({health}:{health: number|undefined}){
     </div>
 
     return <div className="h-[200px] w-36 bg-black/50 shadow-lg shadow-black relative flex items-center justify-center">
-        <PlayingCard interactable={false} cardCode={`${numberToLetter(Math.min(health,14))}H` as CardCode} />
-        {health > 14 && 
-            <PlayingCard interactable={false} cardCode={`${numberToLetter(health-14)}H` as CardCode} className="absolute left-[calc(50%+16px)] top-[calc(50%+8px)] -translate-x-1/2 -translate-y-1/2" />
+        <PlayingCard interactable={false} cardCode={`${numberToLetter(Math.min(health,(health > 14 ? 13 : 14)))}H` as CardCode} />
+        {health > 13 && 
+            <PlayingCard interactable={false} cardCode={`${numberToLetter(health-13)}H` as CardCode} className="absolute left-[calc(50%+16px)] top-[calc(50%+8px)] -translate-x-1/2 -translate-y-1/2" />
         }
     </div>
 }
